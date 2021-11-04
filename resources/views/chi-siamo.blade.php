6f20fe9e4198e4215da3670e4776d10b778effbc
SM Boolean <samuelemadrigali1991@gmail.com>
2021-11-04 13:02:34 +0100

    first commit

---
+++ resources/views/chi-siamo.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Chi Siamo</title>
+	</head>
+	<body>
+		<h1>Chi Siamo</h1>
+		<ul>
+			<li><a href="/">Home Page</a></li>
+			<li><a href="/chi-siamo">Chi Siamo</a></li>
+			<li><a href="/contatti">Contatti</a></li>
+		</ul>
+	</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $data = [
+        "nome" => "Samuele",
+        "cognome" => "Madrigali",
+        "amici" => [
+            "Federico",
+            "Maurizio"
+        ]
+    ];
+
+    return view('index', $data);
+});
+
+Route::get('/chi-siamo', function() {
+    return view('chi-siamo');
+});
+
+Route::get('/contatti', function() {
+    return view('contatti');
+});
